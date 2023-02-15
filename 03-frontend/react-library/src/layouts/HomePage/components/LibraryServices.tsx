60f3ec180ba3f0996dfe0b7be1b48a3d6126eef0
Zlatka Ciorica <93267452+ZeeCio@users.noreply.github.com>
2023-02-15 11:43:09 +0000

    Add files via upload

---
+++ 03-frontend/react-library/src/layouts/HomePage/components/LibraryServices.tsx
@@ -0,0 +1,22 @@
+export const LibraryServices = () => {
+    return (
+        <div className="container my-5">
+            <div className="row p-4 align-items center border shadow-lg">
+                <div className="col-lg-7 p-3">
+                    <h1 className="display-4 fw-bold">
+                        Can't find what you're looking for?
+                    </h1>
+                    <p className='lead'>
+                        If you cannot find What you're looking for, 
+                        you cansend our library admin's a personal message!
+                    </p>
+                    <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+                        <a className='btn main-color btn-lg text-white' href="#">
+                            Sign up
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
