74671f9d7b37dd1f5fb7acfcf54837ab9bb14acb
Zlatka Ciorica <93267452+ZeeCio@users.noreply.github.com>
2023-02-15 11:33:10 +0000

    Add files via upload

---
+++ 02-backend/spring-boot-library/src/test/java/com/zeecio/springbootlibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeecio.springbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
